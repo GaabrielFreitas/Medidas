8d3d97e21a4c2fdea74ecea2f48f1a6771a8d0ea
unknown <freitascontato2004@gmail.com>
2022-09-24 21:42:22 -0300

    Resolução do exercício Medidas

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿double metros, centimetros, quilometros;
+
+Console.Write("Entre com a medida (em metros): ");
+
+metros = Convert.ToDouble(Console.ReadLine());
+
+centimetros = metros * 100;
+quilometros = metros / 1000;
+
+Console.WriteLine("--- Equivalência ---");
+Console.WriteLine($"{centimetros} centimetros");
+Console.WriteLine($"{metros} metros");
+Console.WriteLine($"{quilometros} quilometros");
